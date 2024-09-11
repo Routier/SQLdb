a4009fb92efef252409def4bdc123a14223c6b43
num13 <num13-ne@mail.ru>
2024-09-11 11:55:34 +0300

    Initial Commit

---
+++ src/test/java/Test/BankLoginTest.java
@@ -0,0 +1,58 @@
+package Test;
+
+import Data.DataHelper;
+import Data.SQLHelper;
+import Pages.LoginPage;
+import org.junit.jupiter.api.*;
+
+import static Data.SQLHelper.cleanAuthCodes;
+import static Data.SQLHelper.cleanDataBase;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankLoginTest {
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDataBase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Success login")
+    void shouldSuccessfulLogin() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    @DisplayName("Error : no user")
+    void shouldGetErrorNotificationIfNoUserInDB() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Error : user - wrong verification code")
+    void shouldGetErrorNotificationIfWrongVerificationCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз.");
+    }
+}
